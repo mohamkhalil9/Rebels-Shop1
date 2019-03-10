275fb62c8581f097185ec89542f0e3896f2f45cb
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-03-10 20:19:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
-const devs = ['389090790984515594'];
+const devs = ['415142691282616330'];
 const db = require('quick.db');
-const premium = ['470896018603376640']
+const premium = ['554366616239079464']
 const client = new Discord.Client();   
 const bot = new Discord.Client();   
 const giphy = require('giphy-api')();    
